a2bd8fe548e0de609c1c4222346b08cd62dce14b
sienkie <sienkiewicz.karolina2@gmail.com>
2017-12-05 01:16:43 +0100

    bedgraph input, revert color scale, chromosome territories map

--- hilbert.py
+++ hilbert.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 import numpy as np
 from scipy.interpolate import interp1d
 
@@ -64,23 +64,25 @@ def id_to_xy(id, order=depth):
 
 
 def get_spaced_colors(n):
-    max_value = 16581375  # 255**3
+    max_value = 255 ** 3
     interval = int(max_value / n)
     colors = [hex(I)[2:].zfill(6) for I in range(0, max_value, interval)]
 
     return [(int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)) for i in colors]
 
 
-def hilbert(chromSizes, files, output_file, create_map=False, threshold=0, size=2 ** depth):
+def hilbert(chromSizes, files, output_file, create_map=False, revert=False, threshold=0, size=2 ** depth):
     assert len(files) <= 3
 
-    chrom_sizes = {}
+    filetypes = [kind.split(".")[-1] for kind in files]
+    for kind in filetypes:
+        assert kind in ['bed', 'bedgraph', 'bdg']
+
     with open(chromSizes, 'r') as f:
-        chrom_sizes = {str(line.split()[0]): int(line.split()[1]) for line in f}
+        chromlines = f.readlines()
+        chrom_sizes = {str(line.split()[0]): int(line.split()[1]) for line in chromlines}
+        chrs = [line.split()[0] for line in chromlines]
     whole_genome = sum([v for (k, v) in chrom_sizes.items()])
-    chrs = list(chrom_sizes.keys())
-    chrs.sort(key=lambda item: (len(item), item))
-    # TODO other chromosome order?? now: chr1 .. chr9, chrX, chrY, chr10 .. chr21
 
     data = np.zeros((size, size, 3), dtype=np.uint8)  # default black pixels
 
@@ -90,20 +92,20 @@ def hilbert(chromSizes, files, output_file, create_map=False, threshold=0, size=
     for i in range(len(files)):
 
         f = open(files[i], 'r')
-        flines = f.readlines()[1:]  # TODO optional header ??
+        flines = f.readlines()
+        if flines[0].split()[0] in ["track", "browser"]:  # TODO optional header ??
+            flines = flines[1:]
         f.close()
-
-        max_score = max([float(line.split()[4]) for line in flines])
-        interpolateColor = lambda x, y: interp1d([threshold, max_score], [x, y])  # interpolate to colors on x,y scale
 
         for line in flines:
             l = line.split()
-            if len(l) > 4:
-                print(l)
+            minlen = 5 if filetypes[i] == "bed" else 4
+            if len(l) >= minlen:
+                print(l)  # TODO remove
                 chr = l[0]
                 if chr in chrs:
                     relative_position = sum([chrom_sizes[chrs[c]] for c in range(0, chrs.index(chr))])
-                    score = float(l[4])
+                    score = float(l[4]) if filetypes[i] == "bed" else float(l[3])
                     start = int(l[1]) + relative_position
                     stop = int(l[2]) + relative_position
                     if score > threshold:
@@ -112,9 +114,45 @@ def hilbert(chromSizes, files, output_file, create_map=False, threshold=0, size=
                         for point in points:
                             data[point][i] = 255
 
-        if create_map:
-            pass
+        img = Image.fromarray(data, 'RGB')
+        if revert:
+            img = ImageOps.invert(img)
+        img.save(output_file)
 
-    img = Image.fromarray(data, 'RGB')
-    img.save(output_file)
-    # img.show()
+        if create_map:
+            data_colors = np.zeros((size, size, 3), dtype=np.uint8)  # default black pixels
+            colors = get_spaced_colors(len(chrs) + 1)[1:]
+
+            for i in range(len(chrs)):
+                print(i)
+                chr = chrs[i]
+                start = sum([chrom_sizes[chrs[c]] for c in range(0, chrs.index(chr))]) + 1
+                stop = start + chrom_sizes[chrs[i]] - 1
+                print(start, stop)  # TODO remove
+                print(id_to_xy(int(interpolate2HC(start))), id_to_xy(int(interpolate2HC(stop)) + 1))
+                points = [id_to_xy(px) for px in
+                          range(int(interpolate2HC(start)), int(interpolate2HC(stop)) + 1)]
+                for point in points:
+                    data_colors[point] = colors[i]
+
+            img_col = Image.fromarray(data_colors, 'RGB')
+
+            padding = 5
+            font_size = 50  # TODO: this is default font size, resize if all chromosomes wont fit
+            font = ImageFont.truetype("fonts/THSarabunNew.ttf", font_size)  # TODO: change licence --> GNU font
+            width = max([font.getsize(c)[0] for c in chrs])
+            height = max([font.getsize(c)[1] for c in chrs])
+            img_collist = Image.new('RGB', (width + 2 * padding, size), (0, 0, 0))
+            draw = ImageDraw.Draw(img_collist)
+
+            start = padding
+            for i in range(len(chrs)):
+                draw.text((padding, start), chrs[i], colors[i], font=font)
+                draw = ImageDraw.Draw(img_collist)
+                start += height
+
+            img = Image.new("RGB", (width + 2 * padding + size, size), (0, 0, 0))
+
+            img.paste(img_col, (0, 0))
+            img.paste(img_collist, (size, 0))
+            img.save(output_file.split(".")[0] + "_chrs_territories.png")
